89d902eb768c5089984b63f2662562cac8e33d3b
Salman_JS <salman4javascript@gmail.com>
2023-04-02 14:33:11 +0530

    Added one to many relationship for data and app

--- routes/Routes.js
+++ routes/Routes.js
@@ -7,16 +7,24 @@ const router = express.Router();
 const User = require("../models/User");
 
 // Required Controllers
-const userController = require("../controller/userController")
+const userController = require("../controller/userController");
+const contentController = require("../controller/Content/contentController");
 
 // POST Request
-router.post("/adduser", userController.addUser);
+router.post("/:id/adduser", userController.addUser);
 
 router.post("/:id/updateuser", userController.updateUser);
 
 router.post("/loginuser", userController.loginUser);
 
-// GET Request
+router.post("/:id/deleteuser", userController.deleteUser);
+
+// POST request for data server!
+router.post("/:id/addcontent", contentController.addCs);
+
+// GET Request for all the servers
 router.get("/alluser", userController.allUsers);
+
+router.get("/alldataserver", contentController.allDataServer);
 
 module.exports = router;
